c80e2e968bda30c95384f1747809f8d371830d32
daimaricel <daianamaricel@hotmail.com>
2023-02-10 19:43:08 -0300

    ultimos cambios

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// Control de gastos
+
 class nuevoIngreso{
     constructor(ingreso, categoria){
         this.ingreso= ingreso;
@@ -10,9 +12,6 @@ const plazoFijo = new nuevoIngreso(5000, "intereses");
 
 const arrayIngresos=[sueldo, plazoFijo];
 
-const totalIngresos= arrayIngresos.reduce((acumulador, item) => acumulador + item.ingreso, 0);
-console.log(totalIngresos);
-
 
 class nuevoGasto{
     constructor(gasto, categoria){
@@ -24,18 +23,13 @@ const movistar= new nuevoGasto(1800, "servicios");
 const supermercado = new nuevoGasto(8500, "alimentos");
 
 const arrayGastos= [movistar, supermercado];
-
-const totalGastos = arrayGastos.reduce ((acumulador, item) => acumulador + item.gasto, 0);
-console.log(totalGastos);
-
-
+ 
 //Menu inicial
 
 function menu(){
     let opcion= parseInt(prompt ("Gestión de dinero personal \nIngrese una de las siguientes opciones:\n1) Ingreso de dinero \n2)Gastos \n3)Consultar saldo \n4)Salir"));
     return opcion
 }
-
 
 //Opción 1 - Ingreso de dinero
 
@@ -45,6 +39,7 @@ function ingresoDinero(){
     let ingresa= new nuevoIngreso(ingreso, categoriaIngreso);
     arrayIngresos.push(ingresa);
     console.log(arrayIngresos);
+    alert("El ingreso se realizó con éxito");
  }
 
 //Opción 2 - Gastos
@@ -55,15 +50,23 @@ function ingresoGasto(){
     let ingresoGasto= new nuevoGasto (gasto, categoriaGastos);
     arrayGastos.push(ingresoGasto);
     console.log(arrayGastos);
+    alert("Tu gasto ha sido ingresado");
 }
 
 //Opción 3 - Consultar saldo
 
-const saldo = (a,b) => a-b;
-let saldoActual = saldo(totalIngresos, totalGastos);
-console.log(saldoActual);
+function informe() {
+    
+    const totalIngresos= arrayIngresos.reduce((acumulador, item) => acumulador + item.ingreso, 0);
+    console.log(totalIngresos);
 
-function informe(){
+    const totalGastos = arrayGastos.reduce ((acumulador, item) => acumulador + item.gasto, 0);
+    console.log(totalGastos);
+
+    const saldo = (a,b) => a-b;
+    let saldoActual = saldo(totalIngresos, totalGastos);
+    console.log(saldoActual);
+
     alert("Tu saldo actual es de $" + saldoActual);
 }
 
@@ -73,7 +76,12 @@ function salida(){
     alert("¡Hasta pronto!");
 }
 
-let opcion= menu();
+// Ejecución del menu
+
+let opcion;
+
+do{
+opcion= menu();
 
 switch(opcion){
     case 1:
@@ -97,4 +105,7 @@ switch(opcion){
             break;
 
 } 
+}while (opcion !=4);
+
+
 
